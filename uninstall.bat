1d43d25494fcf451b4645d27b8850c3d7ef4fff0
ClaytonTDM <claytontdm@gmail.com>
2023-01-27 02:53:28 -0500

    Improved installer/uninstaller

--- uninstall.bat
+++ uninstall.bat
@@ -18,24 +18,6 @@ goto nodejs-postinstall
 echo Cleaning Up Files...
 echo.
 title Cleaning Up Files...
-rmdir /s /q .git
-rmdir /s /q .github
-rmdir /s /q .vscode
-rmdir /s /q data_authenticator
-rmdir /s /q data_ykman
-rmdir /s /q assets
-rmdir /s /q node_modules
-rmdir /s /q src
-del index.js
-del package.json
-del package-lock.json
-del start.bat
-del test.bat
-del undecryptable-value-error-fix.bat
-del test.js
-del .gitignore
-del LICENSE
-del README.md
 del %temp%\npm-installed
 cls
 echo Uninstalled.
